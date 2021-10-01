d449322f722d23715523dd3115622b69fa1e02d9
Carolmsm <Carolmsm>
2021-10-01 17:39:15 -0300

    Criação de commit de consulta da conta corrente por usuario

--- src/main/java/matera/bootcamp/projetoPix/rest/status/usuario/UsuarioCrudApiController.java
+++ src/main/java/matera/bootcamp/projetoPix/rest/status/usuario/UsuarioCrudApiController.java
@@ -5,6 +5,7 @@ import matera.bootcamp.projetoPix.domain.model.Usuario;
 import matera.bootcamp.projetoPix.service.usuario.UsuarioCrudService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,12 +37,12 @@ public class UsuarioCrudApiController {
 
     }
     @GetMapping("/api/v1/usuario")
-    public ResponseEntity<Page<Usuario>> buscarPaginado(Pageable pageable){
+    public ResponseEntity<Page<Usuario>> buscarPaginado
+            (@PageableDefault(size = 15, page = 0) Pageable pageable){
 
-      //  var paginaUsuarios : Page<Usuario> = usuarioCrudService
-      //          .buscarPaginado(pageable);
 
-        var paginaUsuarios = usuarioCrudService.buscarPaginado(pageable);
+        var paginaUsuarios = usuarioCrudService
+                .buscarPaginado(pageable);
 
         return  ResponseEntity
                 .status(HttpStatus.OK)
